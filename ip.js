12ff73da6853f782d5552ec5636e462b7ba13249
Ryan Yang <Ryanyang99@hotmail.com>
2018-07-06 12:15:22 -0700

    Update

--- ip.js
+++ ip.js
@@ -2,3 +2,11 @@ $.getJSON('https://ipapi.co/json/', function(data) {
   console.log(JSON.stringify(data, null, 2));
   console.log(data.city)
 });
+
+/* TO just Get City */
+  $.getJSON('https://ipapi.co/json/', function(data) {
+  console.log(data.city);
+  document.getElementById('Location').value = data.city;
+  });
+  
+  
