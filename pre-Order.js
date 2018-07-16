7c4bf31defa24b323b0c72ea3557dd00d57c40c9
Ryan Yang <Ryanyang99@hotmail.com>
2018-07-16 08:51:11 -0700

    Pre-Order Feature

---
+++ pre-Order.js
@@ -0,0 +1,53 @@
+  /*
+    $(document).ready( function() {
+      console.log("hello");
+        $('select').change(function() {
+			console.log("work");
+
+        });
+    });
+*/
+
+  /*
+  function myFunction() {
+    console.log("hello");
+  var preOrder = document.getElementById(7824881811516).selected;
+  
+  if(preOrder){
+	document.getElementById("addToCart").innerHTML = "Pre-Order";
+  }
+  }
+  */
+  
+/*
+    $(document).ready( function() {
+        $('select').change(function() {
+
+            var preOrder = document.getElementById(7824881647676).selected;
+  
+  			if(preOrder){
+			document.getElementById("addToCart").innerHTML = "Pre-Order";
+  			}
+        });
+    });
+  */
+  
+  /*
+              var preOrder = document.getElementById(7824881647676).selected;
+  
+  			if(preOrder){
+			document.getElementById("addToCart").innerHTML = "Pre-Order";
+            }else{
+            document.getElementById("addToCart").innerHTML = "Add to Cart";
+            }
+$("select[value='selector']").change(function() {
+   // alert("test");
+              var preOrder = document.getElementById(7824881647676).selected;
+  
+  			if(preOrder){
+			document.getElementById("addToCart").innerHTML = "Pre-Order";
+            }else{
+            document.getElementById("addToCart").innerHTML = "Add to Cart";
+            }
+});
+*/
