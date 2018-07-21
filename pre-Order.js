234d636cf37f7548f0ef4f4c65d06424bd42cbc5
Ryan Yang <Ryanyang99@hotmail.com>
2018-07-20 23:40:20 -0700

    Added Ajax

--- pre-Order.js
+++ pre-Order.js
@@ -65,3 +65,39 @@ ajax
              alert(status);
         }
     })
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://vesselbags.com/collections/pre-order/products.json',
+        dataType: 'json',
+        success: function(product){
+          console.log(product);
+
+        },
+        error: function(status){
+             alert(status);
+        }
+
+
+    })   
+      //pre-Order right now checks specific id   
+    /*
+    $.ajax({
+        type: 'GET',
+        url: '/products.json',
+        dataType: 'json',
+        success: function(product){
+          console.log(product);
+          console.log(Object.keys(product.products[0].variants).length);
+          console.log(Object.keys(product.products).length); 
+          for(var index = 0; index < Object.keys(product.products).length; index++){//loop through all variant id
+            for(var index2 = 0; index2 < Object.keys(product.products[index].variants).length; index2++){
+              console.log(product.products[index].variants[index2].id);
+            }
+          }
+        },
+        error: function(status){
+             alert(status);
+        }
+    })  
+    */
